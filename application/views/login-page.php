1bf58dba71d9b78bb68ef60f93260c0d9b39cdc2
Thaveesha Dheerasekera <thaveesha.00@gmail.com>
2022-12-25 18:24:02 +0530

    Global styles added to the login page

--- application/views/login-page.php
+++ application/views/login-page.php
@@ -3,43 +3,46 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/login-page/login-page-style.css'); ?>" media="all">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/login-page/login-page-style.css">
+    <link rel='stylesheet' type='text/css' href="<?php echo base_url(); ?>public/css/global/form-style.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/global/global-style.css">
-  </head> 
-  
-  <body>
+</head>
+
+<body>
     <div class="container">
-      <div class="info-section">
-      <img src="<?php echo base_url(); ?>public/assets/images/logo.png" alt="Logo">
-        <h1>Developer Support</h1>
-      </div>
-      <div class="vl"></div>
-      <div class="form-setion">
-        <div class="header">
-          <h2>Login</h2>
+        <div class="info-section">
+            <img src="<?php echo base_url(); ?>public/assets/images/logo.png" alt="Logo">
+            <h1>Developer Support</h1>
         </div>
-        <div class="input-section">
-          <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" name="username" class="form-control">
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" name="password" class="form-control">
-          </div>
-        </div>
-        <div class="btn-panel-row">
-          <button class="btn sign-up-btn">Sign Up</button>
-          <div class="btn-panel-column">
-            <div class="login-cancel-btns">
-              <button class="btn cancel-btn">Cancel</button>
-              <button class="btn login-btn">Login</button>
+        <div class="vl"></div>
+        <div class="form-setion">
+            <div class="header">
+                <h2>Login</h2>
             </div>
-          <a href="#">Forgot Password</a>
-          </div>
+            <div class="input-section">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+            </div>
+            <div class="btn-panel-row">
+                <button class="btn sign-up-btn">Sign Up</button>
+                <div class="btn-panel-column">
+                    <div class="login-cancel-btns">
+                        <button class="btn cancel-btn">Cancel</button>
+                        <button class="btn login-btn">Login</button>
+                    </div>
+                    <a href="#">Forgot Password</a>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </body>
+</body>
+
 </html>
